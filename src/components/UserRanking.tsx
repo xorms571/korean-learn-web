644c5efb6773b211252f1088cf6165fbe3a446f9
xorms571 <xorms6865@naver.com>
2025-09-14 17:20:09 +0900

    edit user ranking component

--- src/components/UserRanking.tsx
+++ src/components/UserRanking.tsx
@@ -61,19 +61,22 @@ export default function UserRanking() {
         <ul className="space-y-3">
           {topUsers.map((user, index) => (
             <li key={user.uid} className="flex items-center justify-between p-2 rounded-md hover:bg-gray-50">
-              <div className="flex items-center">
-                <span className="font-bold text-gray-700 mr-3">#{index + 1}</span>
-                {user.photoURL ? (
-                  <img src={user.photoURL} alt={user.displayName} className="w-8 h-8 rounded-full mr-3" />
-                ) : (
-                  <div className="w-8 h-8 rounded-full bg-gray-200 flex items-center justify-center mr-3 text-gray-500 text-sm">
-                    {user.displayName ? user.displayName.charAt(0) : 'U'}
-                  </div>
-                )}
-                <span className="font-medium text-gray-800 text-nowrap truncate w-14">{user.displayName}</span>
+              <div>
+                <div className="flex items-center">
+              <span className="font-bold text-gray-700 mr-3 w-7 text-center">#{index + 1}</span>
+                  {user.photoURL ? (
+                    <img src={user.photoURL} alt={user.displayName} className="w-8 h-8 rounded-full mr-3" />
+                  ) : (
+                    <div className="w-8 h-8 rounded-full bg-gray-200 flex items-center justify-center mr-3 text-gray-500 text-sm">
+                      {user.displayName ? user.displayName.charAt(0) : 'U'}
+                    </div>
+                  )}
+                  <span className="font-medium text-gray-800 text-nowrap truncate w-14">{user.displayName}</span>
+                </div>
               </div>
               <div className="text-right flex flex-col w-[63.7px]">
-                <span className="block text-sm text-blue-600 font-semibold text-nowrap">{user.completedLessons} courses</span>
+                <span className='text-blue-500 text-sm font-semibold text-nowrap'>{user.currentLevel}</span>
+                <span className="block text-sm text-blue-400 text-nowrap">{user.completedLessons} courses</span>
                 <span className="block text-xs text-gray-500 text-nowrap">{user.totalIndividualLessonsCompleted || 0} lessons</span>
               </div>
             </li>
